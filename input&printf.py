0452d17e3d5b0fdfcd5f8e8f5b44e2c11bc2b194
Douglas <114787695+DouglasalvesUwU@users.noreply.github.com>
2024-07-02 21:31:45 -0300

    02/07/2024

---
+++ input&printf.py
@@ -0,0 +1,34 @@
+#Boa noite, segue nosso primeiro programa de forma autonomo.
+#print ('olá,qual é o seu Nome: ')
+#print que serve para exibir a tela e temos o input
+#name = input ('pode digitar o seu nome ?:')
+#input, serve para inserir texto via console.
+
+#print(' Olá ' ,name, ' seja bem vindo ao nosso App')
+
+##faça um
+
+
+
+
+#Nome = input (' pode digitar seu Nome :')
+#Cpf = input (' pode digitar seu Cpf :')
+#Cep = input (' pode digitar seu endereço :')
+#Email = input ('pode digitar o seu Email :')
+
+
+#print('seja bem vindo ' ,Nome, ' seu cpf ',Cpf,' seu endereço ',Cep,' Seu gamil',Email,'ao nosso App')
+
+#print('preencha as informaço~es abaixo para completa seu cadastro')
+#name = str(input)'digite seu nome inteiro: '))
+#email = str(input)'digite seu e-mail: '))
+#cpf = int(input('digite seu cpf : '))
+#cep = int(input('digite seu cep : '))
+#print ('parabéns {} seu cadastro fooi realizado com sucesso. \nAqui estão seu dados \nnomes: {}\ne-mail: {}\ncep: {}'.format(name, name , email , cpf ,cep))
+
+print('preencha as informaço~es abaixo para completa seu cadastro')
+name = str(input ('digite seu nome inteiro: '))
+email = str(input ('digite seu e-mail: '))
+cpf = int(input('digite seu cpf : '))
+cep = int(input('digite seu cep : '))
+print ('parabéns {} seu cadastro fooi realizado com sucesso. \nAqui estão seu dados \nnomes: {}\ne-mail: {}\ncep: {}'.format(name, name , email , cpf ,cep))
