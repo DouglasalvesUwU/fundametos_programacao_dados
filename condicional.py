93b673e569787e68c655dc7a1561dcb1c1ed1124
Douglas <114787695+DouglasalvesUwU@users.noreply.github.com>
2024-07-03 21:39:22 -0300

    Add files via upload

---
+++ condicional.py
@@ -0,0 +1,39 @@
+
+#idade = 16
+
+#if idade == 16:
+# if idade pra menor de idade 16 incial 
+#igual menor de idade 
+ #print ('menor de idade')
+
+#idade = int(input('digite a sua idade:)
+#if idade <= 16
+# print ('você é menor de idade')
+
+
+#year = input ('digite a sua idade:')
+
+#if idade <= 16:
+
+ #print ('você é menor de idade') 
+
+#else:
+
+ #print ('você é maior de idade') 
+
+FaixaEtaria = int(input('digite a sua idade: '))
+
+if FaixaEtaria >18 :
+#if pra maior de idade com 18 anos de idade codigo primero
+ print  ('filme terror ')
+
+elif FaixaEtaria  >=14:
+ #faixaetaria de filmes filme ação pra idade de 14 anos segundo
+ print  ('filme ação')
+
+
+else:
+ 
+  print('filme infantil')
+
+#else causa pra filme infatil 
---
+++ laços de repetição.py
@@ -0,0 +1,21 @@
+#x = 0 
+
+#while x < 5:
+ #nome = input ('Qual é o seu nome')
+ 
+ #x=x+1
+
+
+x = 0
+
+animais = []
+
+while x < 5:
+
+animais= input ('Qual é o seu animais')
+
+animais.append()
+
+x=x+1
+
+print(animais)
